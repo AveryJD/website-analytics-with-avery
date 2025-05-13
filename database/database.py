b68df4243d07ee28667e97855d97c84f68712c91
AveryJD <averyjdoiron@icloud.com>
2025-05-13 01:55:40 -0300

    Update database.py and templates
    Change summary column name to preview

--- database/database.py
+++ database/database.py
@@ -3,7 +3,7 @@ import os
 from contextlib import contextmanager
 
 DATABASE_NAME = 'database/blog_posts.db'
-POSTS_FOLDER = 'posts'
+POSTS_FOLDER = 'static/posts'
 
 
 def create_tables():
@@ -14,7 +14,7 @@ def create_tables():
                 filename TEXT NOT NULL,
                 title TEXT NOT NULL,
                 date TEXT NOT NULL,
-                summary TEXT NOT NULL,
+                preview TEXT NOT NULL,
                 content TEXT NOT NULL
             )
         ''')
@@ -34,20 +34,25 @@ def get_all_posts():
     with get_db() as conn:
         return conn.execute('SELECT * FROM blog_posts').fetchall()
     
-def create_post(filename, title, date, summary, content):
+def create_post(filename, title, date, preview, content):
     with get_db() as conn:
         conn.execute(
-            'INSERT INTO blog_posts (filename, title, date, summary, content) VALUES (?, ?, ?, ?, ?)', 
-            (filename, title, date, summary, content)
+            'INSERT INTO blog_posts (filename, title, date, preview, content) VALUES (?, ?, ?, ?, ?)', 
+            (filename, title, date, preview, content)
         )
 
-def delete_post_by_filename(filename):
+def delete_all_posts():
     with get_db() as conn:
-        conn.execute('DELETE FROM blog_posts WHERE filename = ?', (filename,))
+        conn.execute('DELETE FROM blog_posts')
         conn.commit()
 
 
+
 def import_posts():
+
+    # Reset posts table
+    delete_all_posts()
+
     for filename in os.listdir(POSTS_FOLDER):
         if filename.endswith('.md') or filename.endswith('.txt'):
             filepath = os.path.join(POSTS_FOLDER, filename)
@@ -57,9 +62,8 @@ def import_posts():
             filename = filename.rsplit('.', 1)[0]
             title = lines[0].strip()                # First line = title
             date = lines[1].strip()                 # Second line = date
-            summary = lines[2].strip()              # Third line = summary
+            preview = lines[2].strip()              # Third line = preview
             content = ''.join(lines[3:]).strip()    # Everything after = content
 
-            delete_post_by_filename(filename)
-            create_post(filename, title, date, summary, content)
+            create_post(filename, title, date, preview, content)
             print(f"Imported: {title}")
